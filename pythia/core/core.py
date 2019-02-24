846cbf22ea6105bcfd6344629633cbc235480486
David Cannings <david@edeca.net>
2019-02-24 14:50:50 +0000

    Add license parsing

--- pythia/core/core.py
+++ pythia/core/core.py
@@ -1,4 +1,5 @@
 import logging
+from binascii import unhexlify
 from .windows import PEHandler
 
 
@@ -40,3 +41,54 @@ class DelphiParser(object):
             elif debug > 1:
                 self.logger.setLevel(logging.DEBUG)
 
+
+class DelphiProgram(object):
+
+    license = None
+    units = None
+    classes = None
+
+
+class License(object):
+    """
+    Represents a Delphi license, and can convert raw DVCLAL data to Delphi
+    version information.
+    """
+
+    license_type = None
+
+    _known_licenses = {
+        "Standard": unhexlify("23785D23B6A5F31943F3400226D111C7"),
+        "Professional": unhexlify("A28CDF987B3C3A7926713F090F2A2517"),
+        "Enterprise": unhexlify("263D4F38C28237B8F3244203179B3A83"),
+    }
+
+    def __init__(self, raw_data=None):
+        self.logger = logging.getLogger("pythia.{}".format(self.__class__.__name__))
+
+        if raw_data:
+            self._from_bytes(raw_data)
+
+    def _from_bytes(self, data):
+        """
+        Convert a stream of bytes to a license version (Standard, Professional
+        or Enterprise) or None if the license is not recognised.
+        """
+
+        for version, raw in self._known_licenses.items():
+            if raw == data:
+                self.license_type = version
+                return
+
+        # TODO: Support "fake" Delphi licenses, where the author has calculated
+        #       custom values.  Find some test samples to use.  See:
+        #       https://stackoverflow.com/questions/18720045/what-are-the-list-of-all-possible-values-for-dvclal
+        raise AttributeError("Did not recognise raw data as a valid license")
+
+
+class DelphiClass(object):
+    pass
+
+
+class DelphiUnit(object):
+    pass
