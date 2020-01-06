dd2faa254107569bbce0e3e1feee52bd8f6f433b
David Cannings <david@edeca.net>
2020-01-06 16:23:08 +0000

    Add comments to IDA script

--- utils/pythia_ida_json_loader.py
+++ utils/pythia_ida_json_loader.py
@@ -2,30 +2,38 @@ from __future__ import print_function
 import idaapi
 import json
 
+print("[+] Loading RTTI information from JSON file")
+
 with open("output.json", "r") as fh:
     data = json.load(fh)
+
+print("[+] Finished loading, starting to rename")
 
 for item in data["items"]:
     MakeComm(item['va'], item['name'].encode("ascii"))
 
+    # A short Pascal string (1 byte length, ASCII data, no null terminator).
+    # Note it seems these can be UTF in modern Delphi, e.g. object names.
     if item["type"] == "p":
         MakeUnknown(item["va"], item["size"], DOUNK_SIMPLE)
-        #res = MakeStr(item["va"], item["va"] + item["size"])
         idaapi.make_ascii_string(item["va"], item["size"], ASCSTR_PASCAL)
-        #print("pascal {}".format(res))
     
+    # 4 byte integer value
     elif item["type"] == "I":
         MakeUnknown(item["va"], item["size"], DOUNK_SIMPLE)
         MakeDword(item["va"])
 
+    # 2 byte integer value
     elif item["type"] == "H":
         MakeUnknown(item["va"], item["size"], DOUNK_SIMPLE)
         MakeWord(item["va"])
 
+    # 1 byte value, either integer or a single byte
     elif item["type"] == "B":
         MakeUnknown(item["va"], item["size"], DOUNK_SIMPLE)
         MakeByte(item["va"])
 
+    # GUID, custom format
     elif item["type"] == "G":
         MakeUnknown(item["va"], item["size"], DOUNK_SIMPLE)
         MakeDword(item["va"])
@@ -52,6 +60,7 @@ for item in data["name_hints"]:
     MakeNameEx(item["va"], str(item["name"]), SN_NOWARN)
     seen.add(item["va"])
 
-print("Finished renaming")
+print("[+] Finished renaming")
 
+# This seems to occasionally crash IDA, figure out why before uncommenting
 #idaapi.refresh_strlist(0, 1)
